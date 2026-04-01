ab5897bc2a551e414eb8b0fe7354696d4894182c
Rahkonen <Veeti.Rahkonen@seamk.fi>
2026-04-01 09:19:43 +0300

    poweruppien spawn chance korjaus

--- powerups.py
+++ powerups.py
@@ -109,11 +109,11 @@ class PowerUpManager:
     def __init__(self):
         self.powerups = []
         self.spawn_chances = {
-        "jumpboost": 0,
-        "jetpack": 0,
-        "shoes": 20,
-        "umbrella": 0,
-        None: 80
+        "jumpboost": 2.5,
+        "jetpack": 2.5,
+        "shoes": 2.5,
+        "umbrella": 2.5,
+        None: 90
 }
 
     def spawn_on_platform(self, platform):
